a8c7045e333b545eeed0b58d09c03244f9bed7f7
gabriel.rodrigues1 <gabriel.rodrigues1@hotmail.com>
2018-07-24 21:06:17 -0300

    Initial commit

---
+++ js/Principal.js
@@ -0,0 +1,36 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+console.log(titulo.textContent);
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var peso = paciente.querySelector(".info-peso").textContent;
+var altura = paciente.querySelector(".info-altura").textContent;
+var imc = peso / (altura * altura);
+
+paciente.querySelector(".info-imc").textContent = imc;
+console.log(imc);
+
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
+
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
+
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
+    var imc = paciente.querySelector(".info-imc");
+
+    if (peso > 200 || peso < 0) {
+        imc.textContent = "Peso ínvalido!"
+    }
+    else if(altura > 3.20 || altura < 0){
+        imc.textContent = "Altura ínvalida!"
+    }
+    else{
+        var valorImc = peso / (altura * altura);
+        imc.textContent = valorImc.toFixed(2);
+        console.log(valorImc);
+    }
+}
+
