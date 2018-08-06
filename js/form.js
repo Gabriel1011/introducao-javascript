50bb42099dbb143f8ef1a28ac3e8fd14a18d19d8
Gabriel1011 <38117500+Gabriel1011@users.noreply.github.com>
2018-08-06 09:52:36 -0300

    Functions

--- js/form.js
+++ js/form.js
@@ -4,16 +4,25 @@ var btnAdcionarPaciente =
 
 function adicionarPaciente() {
     event.preventDefault();
-    var pacienteTr = document.createElement("tr");
     var formAdicoinar = document.querySelector("#form-adicionar");
+    
     var paciente = obterDadosDoForm(formAdicoinar);
+    var pacienteTr = MontarTr(paciente, pacienteTr);
 
+    var tabelaPaciente = document.querySelector("#tabela-pacientes");
+    tabelaPaciente.appendChild(pacienteTr);
+}
+
+
+function MontarTr(paciente) {
+
+    var pacienteTr = document.createElement("tr");
     var nomeTd = document.createElement("td");
     var pesoTd = document.createElement("td");
     var alturaTd = document.createElement("td");
     var gorduraTd = document.createElement("td");
     var imcTd = document.createElement("td");
-
+    
     nomeTd.textContent = paciente.nome;
     pesoTd.textContent = paciente.peso;
     alturaTd.textContent = paciente.altura;
@@ -26,10 +35,8 @@ function adicionarPaciente() {
     pacienteTr.appendChild(gorduraTd);
     pacienteTr.appendChild(imcTd);
 
-    var tabelaPaciente = document.querySelector("#tabela-pacientes");
-    tabelaPaciente.appendChild(pacienteTr);
+    return pacienteTr;
 }
-
 
 function obterDadosDoForm(form) {
     var paciente = {
